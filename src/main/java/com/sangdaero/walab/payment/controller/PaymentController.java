0f7aa418ccf4ef1a7546f3d854fc2ea1026ca874
Goinmul <37678912+Goinmul@users.noreply.github.com>
2020-04-30 17:15:10 +0800

    Add files via upload
    added

--- src/main/java/com/sangdaero/walab/payment/controller/PaymentController.java
+++ src/main/java/com/sangdaero/walab/payment/controller/PaymentController.java
@@ -11,19 +11,57 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.sangdaero.walab.common.entity.EventEntity;
+import com.sangdaero.walab.common.entity.FundraisingEntity;
+import com.sangdaero.walab.common.entity.User;
+import com.sangdaero.walab.interest.application.service.InterestService;
+import com.sangdaero.walab.payment.dto.FundraisingDto;
 import com.sangdaero.walab.payment.dto.PaymentDto;
+import com.sangdaero.walab.payment.service.FundraisingService;
 import com.sangdaero.walab.payment.service.PaymentService;
+import com.sangdaero.walab.user.application.dto.SimpleUser;
+import com.sangdaero.walab.user.application.service.UserService;
 
 @Controller
 public class PaymentController {
 
 	private PaymentService mPaymentService;
+	private FundraisingService mFundraisingService;
+	
+	// user mapping part
+	private UserService mUserService;
+	private InterestService mInterestService;
 	
 	// constructor
-	public PaymentController(PaymentService mPaymentService) {
+	public PaymentController(PaymentService mPaymentService, FundraisingService mFundraisingService,
+			UserService mUserService, InterestService mInterestService) {
+		
 		this.mPaymentService = mPaymentService;
+		this.mFundraisingService = mFundraisingService;
+		this.mUserService = mUserService;
+		this.mInterestService = mInterestService;
 	}
 	
+	// - - - - - - - - - - - - Fundraising Part - - - - - - - - - - - - 
+	@GetMapping("/fundraising")
+	public String fundraisingList() {
+		return "html/payment/fundraising/fundraising.html";
+	}
+
+	@GetMapping("/fundraisingForm")
+	public String fundraisingWrite() {
+		return "html/payment/fundraising/fundraisingWrite.html";
+	}
+	
+	@PostMapping("/fundraisingWrite")
+	public String fundraisingWrite(FundraisingDto fundraisingDto) {
+//		mFundraisingService.saveFundraising(fundraisingDto);
+		
+		return "redirect:/fundraising";
+	}
+	
+	
+	// - - - - - - - - - - - - Payment Part - - - - - - - - - - - - 
 	@GetMapping("/payment") // list
 	public String paymentList(@RequestParam(value="pageNum", defaultValue="1") Integer pageNum,
 			@RequestParam(value="keyword", defaultValue="") String keyword,
@@ -45,12 +83,12 @@ public class PaymentController {
 		return "html/payment/payment.html";
 	}
 	
+	
 	// Search, sort by
 	@GetMapping("/payment/search")
 	public String search(@RequestParam(value="pageNum", defaultValue="1") Integer pageNum,
 			@RequestParam(value="keyword", defaultValue="") String keyword, 
 			@RequestParam(value="sortBy", defaultValue="2") Byte sortBy, 
-			@RequestParam(value="status", defaultValue="2") Byte status,
 			Model model) {
 		
 		List<PaymentDto> paymentList = mPaymentService.getSearchPaymentList(pageNum, keyword, sortBy);
@@ -62,21 +100,26 @@ public class PaymentController {
 		model.addAttribute("keyword", keyword);
 		model.addAttribute("sortBy", sortBy);
 		model.addAttribute("pageNum", pageNum);
-		model.addAttribute("status", status);
 		
 		return "html/payment/payment.html";
 	}
 	
 	// go to write form
 	@GetMapping("/paymentForm") 
-	public String paymentWrite() {
+	public String paymentWrite(Model model) {
+		
+		List<SimpleUser> managers = mUserService.getSimpleUserList("manager");
+		model.addAttribute("managers", managers);
 
 		return "html/payment/paymentForm.html";
 	}
 	
 	// write action
 	@PostMapping("/paymentForm") 
-	public String paymentWrite(PaymentDto paymentDto) {
+	public String paymentWrite(PaymentDto paymentDto, Long managerId) {
+		
+		User user = mUserService.findUserEntity(managerId);
+		paymentDto.setManager(user);
 		
 		mPaymentService.savePayment(paymentDto);
 		
@@ -91,23 +134,27 @@ public class PaymentController {
 		
 		PaymentDto paymentDto = mPaymentService.getSinglePaymentById(no);
 		model.addAttribute("paymentDto", paymentDto);
-		
 		model.addAttribute("keyword", keyword);
 		model.addAttribute("sortBy", sortBy);
 		model.addAttribute("pageNum", pageNum);
-
-		return "html/payment/paymentDetail.html";
-	}
-	
-	// update action for attribute 'status' 
-	@PutMapping("paymentStatusUpdate/{no}")
-	public String paymentStatusUpdate(@PathVariable("no") Long no) {
 		
-		PaymentDto paymentDto = mPaymentService.getSinglePaymentById(no);
-		mPaymentService.paymentStatusToggle(paymentDto);
-		System.out.println("controller : status update : "+paymentDto.getStatus());
-
-		return "redirect:/payment";
+		// user mapping part
+		List<SimpleUser> simpleUsers = mUserService.getSimpleUserList();
+		model.addAttribute("simpleUserList", simpleUsers);
+		
+		// all fundraising data as list
+		EventEntity eventId = mPaymentService.getSingleEventEntityById(no);
+		List<FundraisingDto> fundraisingDtoList = mFundraisingService.findByEventId(eventId);
+		
+		// for debugging
+		if(!fundraisingDtoList.isEmpty()) {
+			System.out.println(fundraisingDtoList.get(0).getMemo());
+			System.out.println(fundraisingDtoList.get(0).getDonatorPhone());
+		}
+		
+		model.addAttribute("fundraisingList", fundraisingDtoList);
+		
+		return "html/payment/paymentDetail.html";
 	}
 	
 	// go to update form for attribute 'paymentCheck'
@@ -117,12 +164,19 @@ public class PaymentController {
 		PaymentDto paymentDto = mPaymentService.getSinglePaymentById(no);
 		model.addAttribute("paymentDto", paymentDto);
 		
+		List<SimpleUser> managers = mUserService.getSimpleUserList("manager");
+		model.addAttribute("managers", managers);
+		
 		return "html/payment/paymentUpdate.html";
 	}
 	
 	// update action for attribute 'paymentCheck'
 	@PutMapping("/payment/paymentUpdate/{no}") 
-	public String update(PaymentDto paymentDto) { 
+	public String update(@PathVariable("no") Long no, PaymentDto paymentDto, Long managerId) { 
+
+		User user = mUserService.findUserEntity(managerId);
+		paymentDto.setManager(user);
+		
 		mPaymentService.savePayment(paymentDto);
 		
 		return "redirect:/payment";
@@ -139,6 +193,23 @@ public class PaymentController {
 		mPaymentService.paymentCheckBoxToggle(paymentCheckBox);
 		
 		return "redirect:/payment";
+	}
+	
+	// match User - Payment(fundraising)
+	@GetMapping("payment/paymentUserMatching/{no}")
+	public String matchUserAndPayment(@PathVariable("no") Long no, String[] userMatchingCheckBox) {
+		
+		String redirection = "redirect:/paymentDetail/" + no; // go back to a detail page, with id={no}
+		
+		if (userMatchingCheckBox == null) {
+			return redirection;
+		}
+		
+		// checked user's ids are now in String[] userMatchingCheckBox.
+		// also, @PathVariable "no" is EventEntity's id.
+		mPaymentService.fundraisingUserMatching(userMatchingCheckBox, no);
+		
+		return redirection;
 	}
 	
 	@DeleteMapping("/payment/{no}")
